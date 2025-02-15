7da27006e9f5e16e3743f3db485cf76068d7c6ac
khisus19 <khisus19@gmail.com>
2025-02-15 13:43:20 -0600

    Add functionality to log workout data to Sheety with date, time, and exercise details

--- my_workouts_project/main.py
+++ my_workouts_project/main.py
@@ -1,7 +1,9 @@
 import os, requests
+import datetime as dt
 
 APP_ID = "2225448c"
 API_KEY = os.getenv("API_KEY")
+SHEETY_BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
 GENDER = "male"
 WEIGHT_KG = "63"
@@ -25,4 +27,28 @@ data = {
 }
 
 response = requests.post(url=API_ENDPOINT, json=data, headers=headers)
+result = response.json()["exercises"][0]
+
+## ------------- Posting in the sheet --------------- ##
+
+SHEETY_ENDPOINT = "https://api.sheety.co/ce58ec8cd1cb64fb8e4754e155d62c2c/workoutTracking/workouts"
+
+today_date = dt.datetime.now().strftime("%d/%m/%Y")
+now_time = dt.datetime.now().strftime("%X")
+
+sheety_header = {
+    "Authorization": f"Bearer {SHEETY_BEARER_TOKEN}"
+}
+
+sheety_json = {
+    "workout": {
+        "date": today_date,
+        "time": now_time,
+        "exercise": result['user_input'].title(),
+        "duration": result['duration_min'],
+        "calories": result['nf_calories']
+    }
+}
+
+response = requests.post(SHEETY_ENDPOINT, json=sheety_json, headers=sheety_header)
 print(response.text)
